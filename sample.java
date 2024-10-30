d7b5388bdf2d3f8795f466d2be8ebc13ac1d1fbd
geetika <kgeetika25@gmailcom>
2024-10-30 15:25:05 +0530

    sample java program

---
+++ sample.java
@@ -0,0 +1,12 @@
+https://github.com/Geetikakavati/cseb-java.githttps://github.com/Geetikakavati/cseb-java.git
+
+
+
+public class Sample
+{
+	public Static void main(String args[])
+	{
+		System.out.println("kavati geetika")
+	}
+}
+
